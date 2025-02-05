6a105c27aa0cfaf5eab70c9c2f99884bde15181a
sazzad4677 <sazzad4677@gmail.com>
2025-02-05 18:42:48 +0600

    Cors Error Fixing

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,12 @@ app.use((req, res, next) => {
   }
 });
 app.use(cookieParser())
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ['http://localhost:5173', "https://stationary-shop-frontend-silk.vercel.app/"],
+    credentials: true,
+  }),
+);
 app.use('/api', routes);
 
 // Main routes
